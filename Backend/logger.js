56a6dc47295703bd65c23d55be1c03802c26b063
ahmedqarmout <ahmedqarmout2@gmail.com>
2018-01-14 22:28:28 -0500

    init logger

--- Backend/logger.js
+++ Backend/logger.js
@@ -2,14 +2,13 @@ const fs = require('fs');
 const common = require('./common.js');
 
 var dateStamp;
-var logger;
 
 /**
  * print a normal message
  * 
  * @param {string} text 
  */
-exports.log = function (text) {
+exports.info = function (text) {
     init();
 
     var txt = common.formatString('[{0}] log: {1}', [common.getDate(), text]);
@@ -31,11 +30,11 @@ exports.error = function (text) {
 /**
  * initiate the logging with file
  */
-var init = function () {
-    var currentDate = common.getDateByFormat('YYYY-MM-DD');
+const init = function () {
+    const currentDate = common.getDateByFormat('YYYY-MM-DD');
     if (!process.env.DEBUG && dateStamp !== currentDate) {
         dateStamp = currentDate;
-        logger = fs.createWriteStream(__dirname + '/../logs/'+dateStamp+'.log', {'flags':'a'});
+        const logger = fs.createWriteStream(__dirname + '/../logs/'+dateStamp+'.log', {'flags':'a'});
         process.stdout.write = process.stderr.write = logger.write.bind(logger);
         process.on('uncaughtException', function(err) {
                 console.error((err && err.stack) ? err.stack : err);
