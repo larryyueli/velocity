369679012bbee88a87ecff9905b6ebed6a7082bd
Ramy Esteero <esteeroramy@gmail.com>
2018-03-05 22:41:50 -0500

    Adding the request to get data for the outline of the backlog

--- UI/js/tickets/sprint-backlog.js
+++ UI/js/tickets/sprint-backlog.js
@@ -16,10 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-const splithref = window.location.href.split('/');
-const projectId = splithref[4];
-const teamId = splithref[6];
-
 $(function () {
     getBacklog();
 });
