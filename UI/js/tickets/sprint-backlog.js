f27fef2e5e9a5baf52371a9b7eb7c8e6a54e0afb
esteeroramy <esteeroramy@gmail.com>
2018-03-10 21:31:15 -0500

    adding filters to backlog

--- UI/js/tickets/sprint-backlog.js
+++ UI/js/tickets/sprint-backlog.js
@@ -33,11 +33,76 @@ const nameId = '#name';
 const openTicketId = '#openTicket';
 const estimateId = '#estimate';
 
+const searchFilterId = '#searchFilter';
+const assigneeAutocompleteId = '#assigneeAutocomplete';
+const typeSelectionId = '#typeSelection';
+var selectedAssignee = null;
+var usernamesArray = [];
+
+const sprintsLoadId = '#sprintsLoad';
+const sprintHeaderId = '#sprintHeader';
+const sprintBodyId = '#sprintBody';
 
 
 $(function () {
+    $('select').material_select();
+
+    $(typeSelectionId).on('change', function () {
+        startLoad(sprintsLoadId, sprintsListId);
+        displaySprintsList();
+    });
+
+    $(searchFilterId).on('keyup', function () {
+        startLoad(sprintsLoadId, sprintsListId);
+        displaySprintsList();
+    });
+
+    startLoad(sprintsLoadId, sprintsListId);
+    getListOfAssignee();
     getBacklog();
 });
+
+/**
+ * list of possible assignee
+*/
+function getListOfAssignee() {
+    $.ajax({
+        type: 'GET',
+        url: '/project/team/members/list',
+        data: {
+            projectId: projectId,
+            teamId: teamId
+        },
+        success: function (data) {
+            let usersObj = {};
+            let usernameObj = {};
+            for (let i = 0; i < data.length; i++) {
+                let user = data[i];
+                usersObj[`${user.fname} ${user.lname}`] = `/profilePicture/${user.picture}`;
+                usernameObj[`${user.fname} ${user.lname}`] = user.username;
+                usernamesArray.push(user.username);
+            }
+            $(assigneeAutocompleteId).autocomplete({
+                data: usersObj,
+                limit: 20,
+                onAutocomplete: function (val) {
+                    selectedAssignee = usernameObj[val];
+                    startLoad(sprintsLoadId, sprintsListId);
+                    displaySprintsList();
+                },
+                minLength: 0,
+            });
+        },
+        error: function (data) {
+            handle401And404(data);
+
+            endLoad(sprintsLoadId, sprintsListId);
+            const jsonResponse = data.responseJSON;
+            failSnackbar(getErrorMessageFromResponse(jsonResponse));
+        }
+    });
+}
+
 
 function getBacklog() {
     $.ajax({
@@ -51,6 +116,10 @@ function getBacklog() {
             sprintEntryHTML = $(data.sprintEntryHTML);
             ticketEntryHTML = $(data.ticketEntryHTML);
             ticketsList = data.ticketsList;
+
+            // sprintsList.forEach(sprint => {
+            //     sprint.isActive = false;
+            // });
 
             displaySprintsList();
         },
@@ -87,59 +156,63 @@ function displaySprintsList() {
     //     }
     // });
 
-    // endLoad(groupLoadId, groupListId);
+    endLoad(sprintsLoadId, sprintsListId);
 }
 
 function fillSprintsRow(tickets) {
     var bindedRow = sprintEntryHTML;
+    var isActive = false;
+    bindedRow.find(backlogTicketListId).html('');
 
-    bindedRow.find(issueCountId).html('14');
     bindedRow.find(titleId).html('Bonza - 1');
 
     tickets.forEach(ticket => {
-        // if (passGroupFilter(group)) {
+        if (passTicketFilter(ticket)) {
             bindedRow.find(backlogTicketListId).append(fillTicketRow(ticket));
-        // }
+        }
     });
 
-    //bindedRow.find(backlogTicketListId).html('sdfg');
+    bindedRow.find(issueCountId).html(`${tickets.length} ${translate('issuesFound')} (${bindedRow.find(backlogTicketListId).find('li').length} ${translate('total')})`);
+
+    if (bindedRow.find(backlogTicketListId).find('li').length === 0) {
+        bindedRow.find(backlogTicketListId).append(`<p class="center"><i>${translate('noResultsFoundBasedOnSearch')}</i></p>`)
+    }
+
+    // if (tickets.isActive) {
+    //     bindedRow.find(sprintHeaderId).addClass('active');
+    //     bindedRow.find(sprintBodyId)[0].style.display = 'block';
+    // } else {
+    //     bindedRow.find(sprintHeaderId).removeClass('active');
+    //     bindedRow.find(sprintBodyId)[0].style.display = 'none';
+    // }
 
     return bindedRow[0].outerHTML;
 }
 
-// /**
-//  * Filters a group object to match filter parameters
-//  *
-//  * @param {Object} group
-//  */
-// function passGroupFilter(group) {
-//     const size = parseInt($(groupSizeFilterId)[0].value);
-//     const filterText = $(searchGroupFilterId)[0].value.trim().toLowerCase();
+/**
+ * Filters a group object to match filter parameters
+ *
+ * @param {Object} group
+ */
+function passTicketFilter(ticket) {
+    const filterText = $(searchFilterId)[0].value.trim().toLowerCase();
+    const typeValue = parseInt($(typeSelectionId).val());
+    const assignee = selectedAssignee;
 
-//     // Group size filter
-//     if (size && group.members.length !== size) {
-//         return false;
-//     }
+    // Ticket type filter
+    if (typeValue !== -1 && ticket.type !== typeValue) {
+        return false;
+    }
 
-//     // Search filter
-//     if (filterText !== '' &&
-//         group.name.toLowerCase().indexOf(filterText) === -1 &&
-//         group.members.every(user => {
-//             return `${user.fname} ${user.lname} - ${user.username}`.toLowerCase().indexOf(filterText) === -1 &&
-//                 translate(`user${user.type}`).toLowerCase().indexOf(filterText) === -1
-//         })) {
-//         return false;
-//     }
+    // Search filter
+    if (filterText !== '' &&
+        ticket.title.toLowerCase().indexOf(filterText) === -1 &&
+        ticket.displayId.toLowerCase().indexOf(filterText) === -1) {
+        return false;
+    }
 
-//     return true;
-// }
-
-
-
-
-
-
-
+    return true;
+}
 
 
 function fillTicketRow(ticket) {
@@ -148,9 +221,9 @@ function fillTicketRow(ticket) {
     if (ticket.type === 0) {
         bindedRow.find(typeIconId).html('<img src="/img/icon-ladybird.png" alt="" height="25" width="auto">');
     } else if (ticket.type === 1) {
-        bindedRow.find(typeIconId).html('/img/icon-code-file.png" alt="">');
+        bindedRow.find(typeIconId).html('<img src="/img/icon-code-file.png" alt="" height="25" width="auto">');
     }  else if (ticket.type === 3) {
-        bindedRow.find(typeIconId).html('/img/icon-purchase-order.png" alt="">');
+        bindedRow.find(typeIconId).html('<img src=/img/icon-purchase-order.png" alt="" height="25" width="auto">');
     }
 
     if (ticket.priority === 0 ) {
@@ -166,4 +239,21 @@ function fillTicketRow(ticket) {
     bindedRow.find(openTicketId)[0].href = `/project/${projectId}/team/${teamId}/ticket/${ticket._id}`;
 
     return bindedRow[0].outerHTML;
+}
+
+/**
+ * Sets the status of a sprint to opened or closed to save in memory
+ *
+ * @param {Object} clicked
+ */
+function setActive(clicked) {
+    const sprintName = clicked.parent().find('#title').text();
+
+    // const clickedSprint = sprintsList.find(sprint => {
+    //     return sprint.name === sprintName;
+    // });
+    //
+    // if (clickedSprint) {
+    //     clickedSprint.isActive = !clicked.hasClass('active');
+    // }
 }
