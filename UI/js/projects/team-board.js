c580810945f85d24b3e63ec09cab0c158affa4eb
esteeroramy <esteeroramy@gmail.com>
2018-04-04 20:11:12 -0400

    Adding on click to board

--- UI/js/projects/team-board.js
+++ UI/js/projects/team-board.js
@@ -197,7 +197,7 @@ function fillBoardTicketRow(issue) {
     }
 
     bindedRow.attr('id', issue._id);
-    bindedRow.find(displayIdId).html(issue.displayId);
+    bindedRow.find(displayIdId).html(`<a class="ticket-link" target="_blank" href="/project/${projectId}/team/${teamId}/ticket/${issue._id}">${issue.displayId}</a>`);
     bindedRow.find(nameId).html(issue.title);
     bindedRow.find(estimateId).html(issue.points);
     bindedRow.find(imageId).html(`<img class="circle" src="/picture/${issue.assigneePicture}" alt="" height="auto" width="95%" class="profilePic">`);
