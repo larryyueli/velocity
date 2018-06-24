0b09e09e23f562d9e838320b053316870f8e0089
ahmedqarmout2 <ahmedqarmout2@gmail.com>
2018-06-23 21:37:37 -0400

    adding admin permissions to setup script

--- Scripts/setup.js
+++ Scripts/setup.js
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 "use strict";
 
 const fs = require('fs');
+const mongoClient = require('mongodb').MongoClient;
 const rls = require('readline-sync');
 
 const cfs = require('../Backend/customFileSystem.js');
@@ -35,93 +36,120 @@ const setupAdminAccount = function () {
     config.debugMode = true;
     logger.info('Velocity server setup');
 
-    const pathToConfigFile = `${__dirname}/../velocity.config`;
-
-    const username = rls.question('Please enter your username: ');
-    const fname = rls.question('Please enter your first name: ');
-    const lname = rls.question('Please enter your last name: ');
-    let password = rls.question('Enter your password: ', {
-        hideEchoBack: true,
-        mask: '*'
-    });
-    let password2 = rls.question('Confirm your password: ', {
+    const mongoAdminUsername = rls.question('Enter mongo admin username: ');
+    const mongoAdminPassword = rls.question('Enter mongo admin password: ', {
         hideEchoBack: true,
         mask: '*'
     });
 
-    while (password !== password2) {
-        logger.error('Your passwords do not match, please try again.');
-        password = rls.question('Enter your password: ', {
-            hideEchoBack: true,
-            mask: '*'
-        });
-        password2 = rls.question('Confirm your password: ', {
-            hideEchoBack: true,
-            mask: '*'
-        });
-    }
-
-    const user = {
-        username: username,
-        password: password,
-        fname: fname,
-        lname: lname,
-        type: common.userTypes.MODE_SELECTOR.value,
-        status: common.userStatus.ACTIVE.value
-    };
-
-    const newDbName = `velocity_db_${common.getUUID()}`;
-    const newConfig = JSON.stringify({
-        hostName: config.hostName,
-        httpPort: config.httpPort,
-        httpsPort: config.httpsPort,
-        notificationsWSPort: config.notificationsWSPort,
-        maxSessionAge: config.maxSessionAge,
-        db_host: config.db_host,
-        db_port: config.db_port,
-        db_name: newDbName,
-        db_admin_name: config.db_admin_name,
-        db_admin_password: config.db_admin_password,
-        password: config.password
-    }, null, 4);
-
-    fs.writeFile(pathToConfigFile, newConfig, function (err) {
+    const url = `mongodb://${mongoAdminUsername}:${mongoAdminPassword}@${config.db_host}:${config.db_port}`;
+    mongoClient.connect(url, function (err, client) {
         if (err) {
-            logger.error(JSON.stringify(err));
+            logger.error(JSON.stringify(common.getError(1001)));
             process.exit(1);
-        }
+        } logger.error(JSON.stringify('ok'));
 
-        config.db_name = newDbName;
-        logger.info('The new configuration has been saved!');
-        db.initialize(function (err, result) {
+        const adminDb = client.db('admin');
+        const randomDbSuffix = common.getUUID();
+        const newDbName = `velocity_db_${randomDbSuffix}`;
+        const newDbAdmin = `velocity_admin_${randomDbSuffix}`;
+        const velocityUser = {
+            roles: [{ role: 'readWrite', db: newDbName }]
+        };
+        
+        adminDb.addUser(newDbAdmin, config.db_admin_password, velocityUser, function (err, result) {
             if (err) {
                 logger.error(JSON.stringify(err));
+                logger.error(JSON.stringify(common.getError(1001)));
                 process.exit(1);
             }
 
-            logger.info('Connection to Velocity database successful.');
-            cfs.initialize(function (err, result) {
+            const username = rls.question('Please enter your username: ');
+            const fname = rls.question('Please enter your first name: ');
+            const lname = rls.question('Please enter your last name: ');
+            let password = rls.question('Please enter your password: ', {
+                hideEchoBack: true,
+                mask: '*'
+            });
+            let password2 = rls.question('Please confirm your password: ', {
+                hideEchoBack: true,
+                mask: '*'
+            });
+
+            while (password !== password2) {
+                logger.error('Your passwords do not match, please try again.');
+                password = rls.question('Please enter your password again: ', {
+                    hideEchoBack: true,
+                    mask: '*'
+                });
+                password2 = rls.question('Please confirm your password again: ', {
+                    hideEchoBack: true,
+                    mask: '*'
+                });
+            }
+
+            const user = {
+                username: username,
+                password: password,
+                fname: fname,
+                lname: lname,
+                type: common.userTypes.MODE_SELECTOR.value,
+                status: common.userStatus.ACTIVE.value
+            };
+
+            const newConfig = JSON.stringify({
+                hostName: config.hostName,
+                httpPort: config.httpPort,
+                httpsPort: config.httpsPort,
+                notificationsWSPort: config.notificationsWSPort,
+                maxSessionAge: config.maxSessionAge,
+                db_host: config.db_host,
+                db_port: config.db_port,
+                db_name: newDbName,
+                db_admin_name: newDbAdmin,
+                db_admin_password: config.db_admin_password,
+                password: config.password
+            }, null, 4);
+
+            fs.writeFile(`${__dirname}/../velocity.config`, newConfig, function (err) {
                 if (err) {
                     logger.error(JSON.stringify(err));
                     process.exit(1);
                 }
 
-                logger.info('File System exists and seems ok');
-                users.addUser(user, function (err, userObject) {
+                config.db_name = newDbName;
+                logger.info('The new configuration has been saved!');
+                db.initialize(function (err, result) {
                     if (err) {
                         logger.error(JSON.stringify(err));
                         process.exit(1);
                     }
 
-                    logger.info('Successful created mode selector account.');
-                    cfs.mkdir(common.cfsTree.USERS, userObject._id, common.cfsPermission.OWNER, function (err, result) {
+                    logger.info('Connection to Velocity database successful.');
+                    cfs.initialize(function (err, result) {
                         if (err) {
-                            logger.error(err);
+                            logger.error(JSON.stringify(err));
                             process.exit(1);
                         }
 
-                        logger.info('Successful created users directory.');
-                        process.exit(0);
+                        logger.info('File System exists and seems ok');
+                        users.addUser(user, function (err, userObject) {
+                            if (err) {
+                                logger.error(JSON.stringify(err));
+                                process.exit(1);
+                            }
+
+                            logger.info('Successful created mode selector account.');
+                            cfs.mkdir(common.cfsTree.USERS, userObject._id, common.cfsPermission.OWNER, function (err, result) {
+                                if (err) {
+                                    logger.error(err);
+                                    process.exit(1);
+                                }
+
+                                logger.info('Successful created users directory.');
+                                process.exit(0);
+                            });
+                        });
                     });
                 });
             });
